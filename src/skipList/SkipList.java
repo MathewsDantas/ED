6f711b1268cdc2a65c88c55b93663f1299c9616a
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2022-08-16 09:10:29 -0300

    Inserção do RemoveSkipList e aprimoramento do print.

--- src/skipList/SkipList.java
+++ src/skipList/SkipList.java
@@ -10,7 +10,7 @@ public class SkipList {
     private final Object menosInf = Integer.MIN_VALUE;
     private final Object maisInf = Integer.MAX_VALUE;
     private int altura;
-    public ArrayList<Object> arrayprint = new ArrayList<Object>();// apenas para facilitar na impressão da SkipList
+
 
     public SkipList() {
         this.inicio = new QuadNode(menosInf);
@@ -20,7 +20,7 @@ public class SkipList {
         this.altura = 0;
     }
 
-    public QuadNode busca(Object key){ // Caso n ache a chave, retornará a menor chave mais próxima.
+    private QuadNode buscaMaisProx(Object key){ // Caso n ache a chave, retornará a menor chave mais próxima da key.
         QuadNode n = inicio;
         while (n.getAbaixo() != null){
             n = n.getAbaixo();
@@ -29,6 +29,23 @@ public class SkipList {
             }
         }
         return n;
+    }
+
+    public Object busca(Object key){// Na primeira vez que achar a key ele retorna.
+        QuadNode n = inicio;
+        while (n.getAbaixo() != null){
+            n = n.getAbaixo();
+            while ((int)key >= (int)n.getPosterior().getItem()){
+                if((int)key == (int)n.getItem()){// retorna na primeir ocorrência.
+                    return n.getItem();
+                }
+                n = n.getPosterior();
+            }
+        }
+        if (n.getItem() != key){
+            return "NO SUCH KEY";
+        }
+        return n.getItem();
     }
 
     private void novoNivel(int nivel){
@@ -60,10 +77,9 @@ public class SkipList {
     }
 
     public void insert(Object key) {
-        QuadNode noEncontrado = busca(key);
+        QuadNode noEncontrado = buscaMaisProx(key);
         QuadNode NoAbaixo = null;
         int novoNivel = rand();
-        arrayprint.add(key);
         for (int i = 0; i <= novoNivel; i++) {
 
             QuadNode novoNo = new QuadNode(key);
@@ -89,25 +105,60 @@ public class SkipList {
         }
     }
 
+    public void remove(Object key){
+        QuadNode noEncontrado = buscaMaisProx(key);
+        QuadNode aux = noEncontrado;
+        do {
+            aux.getAnterior().setPosterior(aux.getPosterior());
+            aux.getPosterior().setAnterior(aux.getAnterior());
+            aux = aux.getAcima();
+        }while (aux != null);
+
+    }
+
     public void print(){
-        System.out.println("altura :"+altura);
+        ArrayList<Object> arrayPrint = new ArrayList<>();
+        ArrayList<Object> arrayPrint2 = new ArrayList<>();
+        QuadNode auxPrint = inicio;
+        while (auxPrint.getAbaixo() != null){//Vai para o nivel 0 para pegar todos os elementos.
+            auxPrint = auxPrint.getAbaixo();
+        }
+        while (auxPrint != null){// pega todos os elementos.
+            arrayPrint.add(auxPrint.getItem());
+            auxPrint = auxPrint.getPosterior();
+        }
         QuadNode auxV = inicio;
         QuadNode auxH = auxV;
         int nivel = altura;
-
+        System.out.println("altura :"+altura);
         while(auxV != null){
             System.out.print("Lv"+ nivel+" --> ");
+            arrayPrint2.clear();
             while (auxH != null) {
-                if (auxH.getItem() == menosInf){
-                    System.out.print("(-) ");
-                }
-                else if(auxH.getItem() == maisInf ){
-                    System.out.print("(+) ");
-                }
-                else {
-                    System.out.print("| " + auxH.getItem() + " |");
-                }
+                arrayPrint2.add(auxH.getItem());
                 auxH = auxH.getPosterior();
+            }
+            int x = 0;
+            for (int i = 0; i < arrayPrint2.size(); i++) {
+                for (int j = x; j < arrayPrint.size(); j++){
+                    if (arrayPrint.get(j) == arrayPrint2.get(i)){
+                        if (arrayPrint2.get(i) == menosInf){
+                            System.out.print("(-) ");
+                        }
+                        else if(arrayPrint2.get(i) == maisInf ){
+                            System.out.print(" (+)");
+                        }
+                        else {
+                            System.out.print("| " + arrayPrint.get(j) + " |");
+                        }
+                        j = arrayPrint.size();
+                        x++;
+                    }
+                    else if(arrayPrint.get(j) != arrayPrint2.get(i)){
+                        System.out.print("      ");
+                        x++;
+                    }
+                }
             }
             nivel--;
             auxV = auxV.getAbaixo();
--- src/skipList/TestSkipList.java
+++ src/skipList/TestSkipList.java
@@ -6,9 +6,17 @@ public class TestSkipList {
 
         skipList.insert(34);
         skipList.insert(50);
-        skipList.insert(2);
+        skipList.insert(20);
         skipList.insert(37);
         skipList.print();
+        skipList.remove(20);
+        skipList.print();
+        skipList.remove(37);
+        skipList.print();
+        skipList.insert(79);
+        skipList.print();
+        System.out.println("Busca: "+skipList.busca(34));
+        System.out.println("Busca: "+skipList.busca(10));
 
     }
 }
